e87650d51005b5f986019af0964e7240d25fdd63
Mehedi4467 <mehedi446768@gmail.com>
2022-05-04 23:54:20 +0600

    mongo db connect and first api crated

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-const express =require("express");
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 //middleware
 
 app.use(cors());
 app.use(express());
 
-app.get('/',(req,res)=>{
-    res.send("Server is running ");
+//mongo db connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9x7m2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("foodExpress").collection("users");
+//   console.log("db connect");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client
+      .db("electronic_store")
+      .collection("products");
+
+    // get api
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running ");
 });
 
-app.listen(port,()=>{
-    console.log("Serer is running ",port);
+app.listen(port, () => {
+  console.log("Serer is running ", port);
 });
